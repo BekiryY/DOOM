c52279c570a575f6fb49acbb194b0b056dd64f0d
BekirYufka <bekiryufka@bekiryufka.net>
2026-05-18 13:59:43 +0300

    py code update

--- python_codes/run_doom_key_catcher.bat
+++ python_codes/run_doom_key_catcher.bat
@@ -1,4 +1,3 @@
 @echo off
-echo Klavye baglaniyor... FPGA'nin COM portunun key_catcher.py icinde dogru olduguna emin ol (PORT = 'COM6')
-python "C:\Users\musta\Desktop\DOOM\reis\python_codes\key_catcher.py"
-pause
+C:\Users\BEK\AppData\Local\Programs\Python\Python312\python.exe C:\Users\BEK\repos\DOOM\python_codes\key_catcher.py
+pause
